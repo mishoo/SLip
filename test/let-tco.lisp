7b354345797f3cb7ee025a0af0d3faaea3108a5b
Mihai Bazon <mihai.bazon@gmail.com>
2025-03-29 10:17:14 +0200

    recompile

--- test/let-tco.lisp
+++ test/let-tco.lisp
@@ -3,12 +3,20 @@
 (defparameter *dynamic* 'unbound)
 
 (defun next (val)
-  (format t "~A~%" (+ val *dynamic*)))
+  (format t "~A~%" (+ val *dynamic*))
+  'result)
 
 (defun test-let-tco ()
-  (%::%let* ((foo 1)
-             (*dynamic* 10)
-             (bar 2)
-             (baz 3))
-            (next (+ foo bar baz)))
-  (format t "~A~%" *dynamic*))
+  (let* ((foo 1)
+         (*dynamic* 10)
+         (bar 2)
+         (baz 3))
+    (next (+ foo bar baz))))
+
+(defmacro with-body (&body body)
+  `(let ((*dynamic* 'bound))
+     ,@body))
+
+(defun moo ()
+  (with-body
+    (format t "~A~%" *dynamic*)))
