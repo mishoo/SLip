d0edfe0c8eef91e7df4763ee3b453965a9d329e5
Mihai Bazon <mihai.bazon@gmail.com>
2025-06-29 16:57:57 +0300

    Ymacs

--- ide/ide.js
+++ ide/ide.js
@@ -797,7 +797,7 @@ export function make_desktop(load_files = []) {
     });
     var ymacs = THE_EDITOR = window.YMACS = new Ymacs_SL({ ls_keyName: ".slip" });
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        ymacs.setColorTheme([ "ef-elea-dark" ]);
+        ymacs.setColorTheme([ "ef-owl" ]);
     } else {
         ymacs.setColorTheme([ "ef-duo-light" ]);
     }
